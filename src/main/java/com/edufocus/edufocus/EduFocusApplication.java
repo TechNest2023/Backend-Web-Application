4c7453c6651982dd1419c5b45e77725b8deaa0d6
JosehpH <u20211e098@upc.edu.pe>
2023-10-26 19:26:38 -0500

    Start proyect

---
+++ src/main/java/com/edufocus/edufocus/EduFocusApplication.java
@@ -0,0 +1,13 @@
+package com.edufocus.edufocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFocusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduFocusApplication.class, args);
+    }
+
+}
