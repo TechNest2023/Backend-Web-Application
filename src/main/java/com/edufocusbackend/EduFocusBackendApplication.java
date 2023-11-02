08540e97e562580fac2a4b12c3248bc45d68a8d8
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2023-11-02 04:35:06 -0500

    feat: updates

--- src/main/java/com/edufocusbackend/EduFocusBackendApplication.java
+++ src/main/java/com/edufocusbackend/EduFocusBackendApplication.java
@@ -1,12 +1,19 @@
 package com.edufocusbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class EduFocusBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EduFocusBackendApplication.class, args);
     }
 
+    @GetMapping("/Message")
+    public String message(){
+        return "WELCOME FROM JAVA SPRING BOOT TO AZURE - EDUFOCUS";
+    }
 }
